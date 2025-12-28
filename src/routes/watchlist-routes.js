34e9656e661b0e881a46b53000d38a75babbf0f5
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-29 01:27:30 +0300

    add watchlist routes validators

--- src/routes/watchlist-routes.js
+++ src/routes/watchlist-routes.js
@@ -5,8 +5,16 @@ import {
   updateWatchlistItem,
 } from '../controllers/watchlist-controller.js';
 import authMiddleware from '../middlewares/auth-middleware.js';
+import {
+  addToWatchlistSchema,
+  updateWatchlistItemSchema,
+} from '../validators/watchlist-validators.js';
+import validateRequest from '../middlewares/validate-request.js';
 const router = express.Router();
 router.use(authMiddleware);
-router.route('/').post(addToWatchlist);
-router.route('/:id').delete(removeFromWatchlist).put(updateWatchlistItem);
+router.route('/').post(validateRequest(addToWatchlistSchema), addToWatchlist);
+router
+  .route('/:id')
+  .delete(removeFromWatchlist)
+  .put(validateRequest(updateWatchlistItemSchema), updateWatchlistItem);
 export default router;
---
+++ src/validators/watchlist-validators.js
@@ -0,0 +1,46 @@
+//we will define a schema for each route that its req has a body
+import { z } from 'zod';
+
+const addToWatchlistSchema = z.object({
+  movieId: z.string().uuid(),
+
+  status: z
+    .enum(['PLANNED', 'WATCHING', 'COMPLETED', 'DROPPED'], {
+      error: () => ({
+        message:
+          'Status must be one of: PLANNED , WATCHING , COMPLETED , DROPPED',
+      }),
+    })
+    .optional(),
+
+  rating: z.coerce
+    .number()
+    .int('Rating must be an integer')
+    .min(1, 'Rating must be between 1 and 10')
+    .max(10, 'Rating must be between 1 and 10')
+    .optional(),
+
+  notes: z.string().optional(),
+});
+
+const updateWatchlistItemSchema = z.object({
+  status: z
+    .enum(['PLANNED', 'WATCHING', 'COMPLETED', 'DROPPED'], {
+      error: () => ({
+        message:
+          'Status must be one of: PLANNED , WATCHING , COMPLETED , DROPPED',
+      }),
+    })
+    .optional(),
+
+  rating: z.coerce
+    .number()
+    .int('Rating must be an integer')
+    .min(1, 'Rating must be between 1 and 10')
+    .max(10, 'Rating must be between 1 and 10')
+    .optional(),
+
+  notes: z.string().optional(),
+});
+
+export { addToWatchlistSchema, updateWatchlistItemSchema };
