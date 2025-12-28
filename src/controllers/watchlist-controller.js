d852b218330f387556e53f8404233ed7d8ced05a
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-28 22:59:20 +0300

    add removeFromWatchlist and updateWatchlistItem to watchlist-controller

--- src/controllers/watchlist-controller.js
+++ src/controllers/watchlist-controller.js
@@ -1,7 +1,7 @@
 import { prisma } from '../config/db.js';
 import { StatusCodes } from 'http-status-codes';
 const addToWatchlist = async (req, res) => {
-  const { movieId, userId, status, rating, notes } = req.body;
+  const { movieId, status, rating, notes } = req.body;
   try {
     //check if movie does not exist
     const movie = await prisma.movie.findUnique({
@@ -18,7 +18,7 @@ const addToWatchlist = async (req, res) => {
     const isInWatchlisht = await prisma.WatchlistItem.findUnique({
       where: {
         userId_movieId: {
-          userId: userId,
+          userId: req.user.id,
           movieId: movieId,
         },
       },
@@ -32,7 +32,7 @@ const addToWatchlist = async (req, res) => {
     //all is good
     const watchlistItem = await prisma.watchlistItem.create({
       data: {
-        userId,
+        userId: req.user.id,
         movieId,
         status: status || 'PLANNED',
         rating,
@@ -52,11 +52,85 @@ const addToWatchlist = async (req, res) => {
       .json({ message: 'Error while adding movie to watchlist' });
   }
 };
+const removeFromWatchlist = async (req, res) => {
+  const itemId = req.params.id;
+  try {
+    const watchlistItem = await prisma.watchlistItem.findUnique({
+      where: {
+        id: itemId,
+      },
+    });
+    if (!watchlistItem) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'Watchlist item not found' });
+    }
 
-// const addToWatchlist = async (req, res) => {};
+    if (watchlistItem.userId !== req.user.id) {
+      return res
+        .status(StatusCodes.FORBIDDEN)
+        .json({ error: 'Not allowed to delete this watchlist item' });
+    }
 
-// const addToWatchlist = async (req, res) => {};
+    await prisma.watchlistItem.delete({
+      where: {
+        id: itemId,
+      },
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json({ status: 'success', message: 'Movie removed from watchlist' });
+  } catch (error) {
+    console.error('Error while deleting watchlist item:', error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'failed to delete watchlist item' });
+  }
+};
 
-// const addToWatchlist = async (req, res) => {};
+const updateWatchlistItem = async (req, res) => {
+  const itemId = req.params.id;
+  const { rating, notes, status } = req.body;
+  try {
+    const existingItem = await prisma.watchlistItem.findUnique({
+      where: {
+        id: itemId,
+      },
+    });
+    if (!existingItem) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'Watchlist item not found' });
+    }
+    if (existingItem.userId !== req.user.id) {
+      return res
+        .status(StatusCodes.FORBIDDEN)
+        .json({ error: 'Not allowed to update this watchlist item' });
+    }
+    const updateData = {};
+    if (status !== undefined) updateData.status = status.toUpperCase();
+    if (notes !== undefined) updateData.notes = notes;
+    if (rating !== undefined) updateData.rating = rating;
+    const updatedItem = await prisma.watchlistItem.update({
+      data: {
+        ...updateData,
+      },
+      where: {
+        id: itemId,
+      },
+    });
+    return res.status(StatusCodes.OK).json({
+      status: 'success',
+      data: {
+        watchlistItem: updatedItem,
+      },
+    });
+  } catch (error) {
+    console.error('Error while updating watchlist item:', error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'failed to update watchlist item' });
+  }
+};
 
-export { addToWatchlist };
+export { addToWatchlist, removeFromWatchlist, updateWatchlistItem };
